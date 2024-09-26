13780f8cebd70d01b8f16bea0f8f4ddbaf99797e
Степуков Евгений Вячеславович <stepukov@cs.karelia.ru>
2024-09-27 00:27:56 +0300

    update locale

--- task_manager/views.py
+++ task_manager/views.py
@@ -7,17 +7,17 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    extra_context = {'title': "Main page"}
+    extra_context = {'title': _("Main page")}
 
 
 class LoginUser(SuccessMessageMixin, LoginView):
     form_class = AuthenticationForm
     template_name = 'form.html'
-    extra_context = {'title': "Login", 'button_name': 'Login'}
+    extra_context = {'title': _("Login"), 'button_name': _('Log in')}
     success_message = _('You are logged in')
 
 
 class LogoutUser(LogoutView):
     def post(self, request, *args, **kwargs):
-        messages.success(self.request, "You are logged out")
+        messages.success(self.request, _("You are logged out"))
         return super().post(request, *args, **kwargs)
