c714a7c85f039822c1adde73f51455dc84021f63
Степуков Евгений Вячеславович <stepukov@cs.karelia.ru>
2024-09-28 15:45:55 +0300

    update full name for user

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'label', 'status', 'task_perfomer' ]
+        fields = ['name', 'description', 'status', 'task_perfomer', 'label']
         labels = {
             'name': _('Name'),
             'description': _('Description'),
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -5,3 +5,6 @@ class User(AbstractUser):
     def has_tasks(self):
         return (User.objects.get(id=self.id).tasks.exists() or
                 User.objects.get(id=self.id).tasks_created.exists())
+
+    def __str__(self):
+        return self.get_full_name()
