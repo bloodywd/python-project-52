f3cdd109e1b9142bc52c8a3f6ef27f0d8d7b2a4a
Степуков Евгений Вячеславович <stepukov@cs.karelia.ru>
2024-09-28 15:59:38 +0300

    update locale

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class UserForm(UserCreationForm):
@@ -7,6 +9,24 @@ class UserForm(UserCreationForm):
         model = get_user_model()
         fields = ['first_name', 'last_name', 'username',
                   'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].help_text = _('Field is required')
+        self.fields['last_name'].help_text = _('Field is required')
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if not first_name:
+            raise ValidationError(_("First name is required"))
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if not last_name:
+            raise ValidationError(_("Last name is required"))
+        return last_name
+
 
     def clean_username(self):
         return self.cleaned_data["username"]
